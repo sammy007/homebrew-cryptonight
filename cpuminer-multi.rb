7b574028c3bfa1e9c39338bb4e175c192a63dc89
Sammy Libre <sammy007@users.noreply.github.com>
2014-06-09 17:16:00 +0600

    Fix cpuminer-multi formula

--- cpuminer-multi.rb
+++ cpuminer-multi.rb
@@ -7,16 +7,13 @@ class CpuminerMulti < Formula
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "libtool" => :build
-  depends_on "gcc" => :build
   depends_on "curl"
   depends_on "jansson"
 
   def install
-    ENV['CC'] = '/usr/local/bin/gcc-4.8'
-
     system "./autogen.sh"
     system "perl ./nomacro.pl"
-    system "CFLAGS=\"-march=native -D__unix__\" ./configure"
+    system "CFLAGS=\"-march=native\" ./configure"
     system "make"
     bin.install "minerd"
   end
