80fef76b0b4a1d997d18882032284466b617ebb8
Sammy Libre <sammy007@users.noreply.github.com>
2014-09-12 09:36:28 +0600

    Add bitmonero v0.8.8.3 release

--- bitmonero.rb
+++ bitmonero.rb
@@ -2,6 +2,9 @@ require "formula"
 
 class Bitmonero < Formula
   homepage "http://monero.cc"
+  url "https://github.com/monero-project/bitmonero/archive/v0.8.8.3.tar.gz"
+  sha1 "83dd5825f3c221c7f675e6e941eb69e150fa18d4"
+
   head "https://github.com/monero-project/bitmonero.git"
 
   depends_on "cmake" => :build
@@ -11,6 +14,6 @@ class Bitmonero < Formula
   def install
     system "make"
     bin.install "./build/release/src/bitmonerod", "./build/release/src/simplewallet",
-      "./build/release/src/simpleminer", "./build/release/src/connectivity_tool"
+      "./build/release/src/connectivity_tool"
   end
 end
