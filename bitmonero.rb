8e63838b2c31d11077161501e2effe0c0a46144b
Sammy Libre <sammy007@users.noreply.github.com>
2014-09-15 23:05:10 +0600

    Add bitmonero v0.8.8.4 release

--- bitmonero.rb
+++ bitmonero.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Bitmonero < Formula
   homepage "http://monero.cc"
-  url "https://github.com/monero-project/bitmonero/archive/v0.8.8.3.tar.gz"
-  sha1 "83dd5825f3c221c7f675e6e941eb69e150fa18d4"
+  url "https://github.com/monero-project/bitmonero/archive/v0.8.8.4.tar.gz"
+  sha1 "a30a4e7506b3301ead601d7f4f318920f29a4cb1"
 
   head "https://github.com/monero-project/bitmonero.git"
 
