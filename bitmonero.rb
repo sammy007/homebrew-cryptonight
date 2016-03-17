95823d9dfbaac2ec07ec527fac8c2f3ee9c00686
Sammy Libre <sammy007@users.noreply.github.com>
2016-03-17 07:29:48 +0500

    Update bitmonero formula

--- bitmonero.rb
+++ bitmonero.rb
@@ -8,8 +8,8 @@ class Bitmonero < Formula
   end
 
   stable do
-    url "https://github.com/monero-project/bitmonero/archive/v0.9.1.tar.gz"
-    sha256 "a54f6690aff1b7154c12518edd70f8770b436e4af30bc9dcd5d79b0a2713d94c"
+    url "https://github.com/monero-project/bitmonero/archive/v0.9.2.tar.gz"
+    sha256 "100aff68ae56ee4fe1a897e4047cbf4fcc4ab774f8af56be55d9660602e04b68"
   end
 
   depends_on "cmake" => :build
@@ -21,8 +21,8 @@ class Bitmonero < Formula
 
   bottle do
     cellar :any
-    root_url "https://github.com/sammy007/homebrew-cryptonight/releases/download/monero-v0.9.1"
-    sha256 "3803748ff3ab437c9163f7cd58daa9bf2795b82384f52cbe06348fb032d30c43" => :el_capitan
+    root_url "https://github.com/sammy007/homebrew-cryptonight/releases/download/monero-v0.9.2"
+    sha256 "19c08f86b214dc200b43390442676cc5fcbe2ea7d7f77d6fe6aedaf0ed2858ea" => :el_capitan
   end
 
   def install
