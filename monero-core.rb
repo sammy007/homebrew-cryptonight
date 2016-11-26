9387d951fe738cc57aba610e018f0856fe19b6c2
Sammy Libre <sammy007@users.noreply.github.com>
2016-11-27 00:15:49 +0500

    Add monero-core formula

---
+++ monero-core.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class MoneroCore < Formula
+  desc "Monero Core Qt GUI"
+  homepage "https://getmonero.org"
+
+  head do
+    url "https://github.com/monero-project/monero-core.git"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkgconfig" => :build
+  depends_on "boost"
+  depends_on "openssl"
+  depends_on "qt5"
+
+  def install
+    system "./get_libwallet_api.sh"
+    system "./build.sh"
+    prefix.install "build/release/bin/monero-core.app"
+  end
+end
