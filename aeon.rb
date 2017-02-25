7b8d99a8dcba668c2d1b8319fe7f4491f91f401e
Sammy Libre <sammy007@users.noreply.github.com>
2017-02-26 03:17:43 +0500

    Update aeon formula

--- aeon.rb
+++ aeon.rb
@@ -1,9 +1,17 @@
 require "formula"
 
 class Aeon < Formula
-  homepage "https://github.com/aeonix/aeon"
+  homepage "http://aeon.cash"
+  desc "AEON: the secure, private, untraceable cryptocurrency"
 
-  head "https://github.com/aeonix/aeon.git"
+  head do
+    url "https://github.com/aeonix/aeon.git"
+  end
+
+  stable do
+    url "https://github.com/aeonix/aeon/archive/v0.9.7.1.tar.gz"
+    sha256 "2094419097ca6c5c48258e73dc2e0627732714fe00cfe8951469f05f6993280a"
+  end
 
   depends_on "cmake" => :build
   depends_on "boost"
