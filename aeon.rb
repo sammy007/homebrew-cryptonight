6d221cd60a752a59b37387699dfcff96931ac39d
Sammy Libre <sammy007@users.noreply.github.com>
2017-04-23 15:18:36 +0500

    Update aeon formula

--- aeon.rb
+++ aeon.rb
@@ -9,8 +9,8 @@ class Aeon < Formula
   end
 
   stable do
-    url "https://github.com/aeonix/aeon/archive/v0.9.11.0.tar.gz"
-    sha256 "d6716bfb8cdf9927283d5dfc69d6541f4459ade38637475227242ee844a9cd63"
+    url "https://github.com/aeonix/aeon/archive/v0.9.12.0.tar.gz"
+    sha256 "f51ac2e9065ee8001f5f5d74f65ab6ed05aaa114e4ff627ef6de43c2c72a89eb"
   end
 
   depends_on "cmake" => :build
